c40940ff8f8375e232d3fd8c603e0bee7d976dab
aimiovo <aimiovo@126.com>
2024-06-25 05:40:11 +0800

    fix logout bug

--- gyhr-web/src/permission/index.js
+++ gyhr-web/src/permission/index.js
@@ -3,6 +3,9 @@ export default function hasPermission(code) {
     let tag = false;
     // 获取用户当前的所有权限
     let codeList = JSON.parse(sessionStorage.getItem('codeList'))
+    if (codeList==null) {
+        return tag;
+    }
     for (let i = 0; i < codeList.length; i++) {
         if (codeList[i] === code) {
             tag = true;
