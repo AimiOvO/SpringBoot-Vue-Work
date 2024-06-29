36c87782137c907810fd6fcbc234c1fd5b4598d5
aimiovo <aimiovo@126.com>
2024-06-29 22:48:25 +0800

    Optimize code

--- gyhr-web/src/permission/index.js
+++ gyhr-web/src/permission/index.js
@@ -3,8 +3,7 @@ export default function hasPermission(code) {
     let tag = false;
     // 获取用户当前的所有权限
     let codeList = JSON.parse(sessionStorage.getItem('codeList'))
-    if (codeList==null) {
-        return tag;
-    }
+    if (codeList == null) return tag;
+
     return codeList.includes(code);
 }
